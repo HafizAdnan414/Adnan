32ca9f3df332bdfc5102aa055934c5f2eca42d10
Hafiz Adnan <HafizAdnan414@gmail.com>
2022-03-12 04:41:30 -0800

    first commit

---
+++ app/Http/Controllers/task.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class task extends Controller
+{
+    function show()
+    {
+    return "Hi hafiz ";
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+<h1>Hi jutt acrobat</h1>
